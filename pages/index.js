69dec25d9018a26c1172c61bd62e9f2c943e0e3f
Akaki <Akaki@Pro1.local>
2022-09-10 20:15:34 +0400

    changeBasic

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,23 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Fragment } from 'react'
-import styles from '../styles/Home.module.css'
-import NavBar from '../components/NavBar'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import NavBar from "../components/NavBar";
+import { Fragment } from "react";
+import HelloPage from "../components/HelloPage";
+ import About from "../components/About";
+import Skills from "../components/Skills";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
 
 export default function Home() {
   return (
-  <Fragment>
-    <NavBar />
-  </Fragment>
-  )
+    <Fragment>
+      <NavBar />
+      <HelloPage />
+     <About />
+     <Skills />
+      <Projects />
+     <Contact />
+    </Fragment>
+  );
 }
