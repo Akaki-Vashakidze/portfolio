1cdd844683c5ab05d3d8f2736bf5abf99a9028ab
Akaki <Akaki@Pro1.local>
2022-09-10 15:30:01 +0400

    second

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Fragment } from 'react'
 import styles from '../styles/Home.module.css'
-import NavBar from '../components/navBar'
+import NavBar from '../components/NavBar'
 
 export default function Home() {
   return (
