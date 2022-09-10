e1f2b3c4725f0f2839f4622b71b7a86f760d562f
Akaki <Akaki@Pro1.local>
2022-09-10 19:23:30 +0400

    try3

--- components/Jjj.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Jjj = () => {
-  return (
-    <div>Jhhhhjj</div>
-  )
-}
-
-export default Jjj
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Fragment } from 'react'
-import Jjj from '../components/Jjj'
 import styles from '../styles/Home.module.css'
+import NavBar from '../components/NavBar'
 
 export default function Home() {
   return (
   <Fragment>
-    <Jjj />
+    <NavBar />
   </Fragment>
   )
 }
